ec28e3eb4a1b64ebec9817f02392b6903560df8f
danisefianto <sefiantodani@gmail.com>
2025-04-19 23:10:19 +0700

    fix button navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             </ul>
           <div onClick={handleNav} className='md:hidden'> {nav ? <AiOutlineMenu size={20} />:<AiOutlineClose size={20} />    } </div>
   
-          <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden'  : 'fixed left-[-100%]'}>
+          <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden'  : 'fixed left-[-100%]'}>
           <h1 className='w-full font-bold uppercase text-3xl text-[#00df9a] m-4'>React.</h1>
             <ul className='uppercase p-4'>
                 <li className='p-4 border-b border-gray-600 button'>Home</li>
